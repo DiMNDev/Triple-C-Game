d579bd3320efe8b566ba1eddb3dcbcc2bbd5746b
Joshua Arnold <joshua.arnold@students.snow.edu>
2024-11-15 09:17:31 -0700

    added ChessLayout record from VS in class

--- Chess-Final.Library/Chess.cs
+++ Chess-Final.Library/Chess.cs
@@ -15,13 +15,127 @@ public class Chess : Game
     public Chess() : base(GameType.Chess)
     {
     }
+    private string LayoutPath = "git";
 
     public string Name { get; private set; } = "Chess";
     public GameBoard Board { get; set; }
-    public void LayoutGamePieces() { }
+    public void LayoutGamePieces()
+    {
+
+    }
 }
 
 internal record ChessPieceLayout
 {
+    //PlayerOpponent[] player = {get; set;}
+}
 
+internal record PlayerOpponent
+{
+
+}
+//internal record 
+
+
+public class Rootobject
+{
+    public Class1[] Property1 { get; set; }
+}
+
+public class Class1
+{
+    public Player[] Player { get; set; }
+    public Opponent[] Opponent { get; set; }
+}
+
+public class Player
+{
+    public Pawn[] pawns { get; set; }
+    public Rook[] rooks { get; set; }
+    public Knight[] knights { get; set; }
+    public Bishop[] bishops { get; set; }
+    public Queen[] queen { get; set; }
+    public King[] king { get; set; }
+}
+
+public class Pawn
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class Rook
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class Knight
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class Bishop
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class Queen
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class King
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class Opponent
+{
+    public Pawn1[] pawns { get; set; }
+    public Rook1[] rooks { get; set; }
+    public Knight1[] knights { get; set; }
+    public Bishop1[] bishops { get; set; }
+    public Queen1[] queen { get; set; }
+    public King1[] king { get; set; }
+}
+
+public class Pawn1
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class Rook1
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class Knight1
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class Bishop1
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class Queen1
+{
+    public string x { get; set; }
+    public int y { get; set; }
+}
+
+public class King1
+{
+    public string x { get; set; }
+    public int y { get; set; }
 }
