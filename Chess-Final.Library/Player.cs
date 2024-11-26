09ccb090326fd09ab177d53d668a5bf702c3470f
DiMNDev <Dimndev@gmail.com>
2024-11-25 18:20:58 -0700

    added SetSelected (initial click)

--- Chess-Final.Library/Player.cs
+++ Chess-Final.Library/Player.cs
@@ -8,10 +8,18 @@ public class Player : IPlayer
     public int Wins { get; set; } = 0;
     public int Losses { get; set; } = 0;
     public Guid PlayerID { get; set; }
+    public GamePiece SelectedPiece { get; set; } = null;
+    public event Action GameHasChanged;
 
     public Player(string name, Guid? guid = null)
     {
         Username = name;
         PlayerID = guid ?? Guid.NewGuid();
+    }
+    public void SetSelected(int X, int Y)
+    {
+        Console.WriteLine($"Clicked: ({X},{Y})");
+        SelectedPiece = GamePieces.Where(p => p.CurrentPosition == (((ChessCoordinate)X).ToString(), Y)).FirstOrDefault();
+        GameHasChanged?.Invoke();
     }
 }
